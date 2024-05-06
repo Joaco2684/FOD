89c41789161f72e4ce438c75f8a8418c06ddd307
Joaquin Di Lorenzo <joaquindilo2684@gmail.com>
2024-05-06 16:11:00 -0300

    Ej7 y ej8

--- Prac3/ej6/ej6.pas
+++ Prac3/ej6/ej6.pas
@@ -13,13 +13,55 @@ type
     file_det = File of Integer;
 
 procedure bajaLogica(var mae:file_mae; var det:file_det);
+var
+    regM:prenda;
+    regD:integer;
+    esta:boolean;
+begin
+    reset(mae);
+    reset(det);
+    while not eof(det) do begin
+        read(det, regD);
+        esta := false;
+        while not eof(mae) and not esta do begin
+            read(mae, regM);
+            if regM.cod_prenda = regD then begin
+                regM.stock := regM.stock * -1;
+                seek(mae, filepos(mae) - 1);
+                write(mae, regM);
+                esta := true; 
+            end;
+        end;
+        seek(mae,0);
+    end;
+    close(mae);
+    close(det);
+end;
 
+procedure efectivizarBajas(var mae:file_mae;var nue:file_mae);
+var
+    regM:prenda;
+begin
+    reset(mae);
+    rewrite(nue);
+    while not eof(mae) do begin
+        read(mae,regM);
+        if (regM.cod_prenda > 0) then
+            write(nue, regM);
+    end;
+    close(mae);
+    close(nue);
+end;
 
 var
-    mae:file_mae;
+    mae,nue:file_mae;
     det:file_det;
 begin
     Assign(mae, 'Maestro');
+    Assign(nue, 'Nuevo');
     Assign(det, 'Detalle');
     bajaLogica(mae,det);
+    efectivizarBajas(mae,nue);
+    Erase(mae);
+    rename(nue,'Maestro');
 end.
